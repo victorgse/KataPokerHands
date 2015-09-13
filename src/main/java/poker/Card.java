f46bc33d8143fe4199e7f13260a7b00b3f75ca7f
victorgse <victorgse@gmail.com>
2015-09-13 15:55:48 +0100

    Commit initial project skeleton

---
+++ src/main/java/poker/Card.java
@@ -0,0 +1,9 @@
+package poker;
+
+public class Card {
+	
+    public boolean someCardMethod() {
+        return true;
+    }
+    
+}
---
+++ src/test/java/poker/CardTest.java
@@ -0,0 +1,14 @@
+package poker;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CardTest {
+	
+    @Test
+    public void testSomeLibraryMethod() {
+        Card classUnderTest = new Card();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someCardMethod());
+    }
+    
+}
