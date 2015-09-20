0ebaf11fe9541d59bbad02dd8412cb4a0ee7b8f2
victorgse <victorgse@gmail.com>
2015-09-20 20:22:02 +0100

    Implement card value and suit through enums, in order to restrict their assignable values

--- src/main/java/poker/Card.java
+++ src/main/java/poker/Card.java
@@ -1,9 +1,106 @@
 package poker;
 
 public class Card {
+	
+	public enum Suit {
+		Clubs, Diamonds, Hearts, Spades
+	}
+	
+	public enum Value {
+		Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+	}
 
-    public boolean someCardMethod() {
-        return true;
-    }
+	private Value value;
+	private Suit suit;
+
+	public Card(String card) {
+		this.value = extractValue(card);
+		this.suit = extractSuit(card);
+	}
+
+	private Value extractValue(String card) {
+		char ch = card.charAt(0);
+		Value value;
+
+		switch (ch) {
+		case '2':
+			value = Value.Two;
+			break;
+		case '3':
+			value = Value.Three;
+			break;
+		case '4':
+			value = Value.Four;
+			break;
+		case '5':
+			value = Value.Five;
+			break;
+		case '6':
+			value = Value.Six;
+			break;
+		case '7':
+			value = Value.Seven;
+			break;
+		case '8':
+			value = Value.Eight;
+			break;
+		case '9':
+			value = Value.Nine;
+			break;
+		case 'T':
+			value = Value.Ten;
+			break;
+		case 'J':
+			value = Value.Jack;
+			break;
+		case 'Q':
+			value = Value.Queen;
+			break;
+		case 'K':
+			value = Value.King;
+			break;
+		case 'A':
+			value = Value.Ace;
+			break;
+		default:
+			value = null;
+			break;
+		}
+
+		return value;
+	}
+
+	private Suit extractSuit(String card) {
+		char ch = card.charAt(1);
+		Suit suit;
+
+		switch (ch) {
+		case 'C':
+			suit = Suit.Clubs;
+			break;
+		case 'D':
+			suit = Suit.Diamonds;
+			break;
+		case 'H':
+			suit = Suit.Hearts;
+			break;
+		case 'S':
+			suit = Suit.Spades;
+			break;
+		default:
+			suit = null;
+			break;
+		}
+
+		return suit;
+	}
+
+	public Value getValue() {
+		return value;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
 
 }
--- src/test/java/poker/CardTest.java
+++ src/test/java/poker/CardTest.java
@@ -6,9 +6,60 @@ import static org.junit.Assert.*;
 public class CardTest {
 
     @Test
-    public void testSomeCardMethod() {
-        Card classUnderTest = new Card();
-        assertTrue("someCardMethod should return 'true'", classUnderTest.someCardMethod());
+    public void testCard_getValue() {
+        Card cardUnderTest = new Card("2C");
+        assertEquals(Card.Value.Two, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("3C");
+        assertEquals(Card.Value.Three, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("4C");
+        assertEquals(Card.Value.Four, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("5C");
+        assertEquals(Card.Value.Five, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("6C");
+        assertEquals(Card.Value.Six, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("7C");
+        assertEquals(Card.Value.Seven, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("8C");
+        assertEquals(Card.Value.Eight, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("9C");
+        assertEquals(Card.Value.Nine, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("TC");
+        assertEquals(Card.Value.Ten, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("JC");
+        assertEquals(Card.Value.Jack, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("QC");
+        assertEquals(Card.Value.Queen, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("KC");
+        assertEquals(Card.Value.King, cardUnderTest.getValue());
+        
+        cardUnderTest = new Card("AC");
+        assertEquals(Card.Value.Ace, cardUnderTest.getValue());
     }
+    
+    @Test
+	public void testCard_getSuit() {
+    	Card cardUnderTest = new Card("2C");
+    	assertEquals(Card.Suit.Clubs, cardUnderTest.getSuit());
+    	
+    	cardUnderTest = new Card("2D");
+    	assertEquals(Card.Suit.Diamonds, cardUnderTest.getSuit());
+    	
+    	cardUnderTest = new Card("2H");
+    	assertEquals(Card.Suit.Hearts, cardUnderTest.getSuit());
+    	
+    	cardUnderTest = new Card("2S");
+    	assertEquals(Card.Suit.Spades, cardUnderTest.getSuit());
+	}
 
 }
