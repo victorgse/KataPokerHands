21c2123059e69630cba634e7500d18abbf8ab290
victorgse <victorgse@gmail.com>
2015-09-20 21:20:08 +0100

    Replaces tabs by spaces, in accordance with best practices

--- src/main/java/poker/Card.java
+++ src/main/java/poker/Card.java
@@ -2,105 +2,105 @@ package poker;
 
 public class Card {
 
-	public enum Suit {
-		CLUBS, DIAMONDS, HEARTS, SPADES
-	}
+    public enum Suit {
+        CLUBS, DIAMONDS, HEARTS, SPADES
+    }
 
-	public enum Value {
-		TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
-	}
+    public enum Value {
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
 
-	private Value value;
-	private Suit suit;
+    private Value value;
+    private Suit suit;
 
-	public Card(String card) {
-		this.value = extractValue(card);
-		this.suit = extractSuit(card);
-	}
+    public Card(String card) {
+        this.value = extractValue(card);
+        this.suit = extractSuit(card);
+    }
 
-	private Value extractValue(String card) {
-		char ch = card.charAt(0);
-		Value tempValue;
+    private Value extractValue(String card) {
+        char ch = card.charAt(0);
+        Value tempValue;
 
-		switch (ch) {
-		case '2':
-			tempValue = Value.TWO;
-			break;
-		case '3':
-			tempValue = Value.THREE;
-			break;
-		case '4':
-			tempValue = Value.FOUR;
-			break;
-		case '5':
-			tempValue = Value.FIVE;
-			break;
-		case '6':
-			tempValue = Value.SIX;
-			break;
-		case '7':
-			tempValue = Value.SEVEN;
-			break;
-		case '8':
-			tempValue = Value.EIGHT;
-			break;
-		case '9':
-			tempValue = Value.NINE;
-			break;
-		case 'T':
-			tempValue = Value.TEN;
-			break;
-		case 'J':
-			tempValue = Value.JACK;
-			break;
-		case 'Q':
-			tempValue = Value.QUEEN;
-			break;
-		case 'K':
-			tempValue = Value.KING;
-			break;
-		case 'A':
-			tempValue = Value.ACE;
-			break;
-		default:
-			tempValue = null;
-			break;
-		}
+        switch (ch) {
+        case '2':
+            tempValue = Value.TWO;
+            break;
+        case '3':
+            tempValue = Value.THREE;
+            break;
+        case '4':
+            tempValue = Value.FOUR;
+            break;
+        case '5':
+            tempValue = Value.FIVE;
+            break;
+        case '6':
+            tempValue = Value.SIX;
+            break;
+        case '7':
+            tempValue = Value.SEVEN;
+            break;
+        case '8':
+            tempValue = Value.EIGHT;
+            break;
+        case '9':
+            tempValue = Value.NINE;
+            break;
+        case 'T':
+            tempValue = Value.TEN;
+            break;
+        case 'J':
+            tempValue = Value.JACK;
+            break;
+        case 'Q':
+            tempValue = Value.QUEEN;
+            break;
+        case 'K':
+            tempValue = Value.KING;
+            break;
+        case 'A':
+            tempValue = Value.ACE;
+            break;
+        default:
+            tempValue = null;
+            break;
+        }
 
-		return tempValue;
-	}
+        return tempValue;
+    }
 
-	private Suit extractSuit(String card) {
-		char ch = card.charAt(1);
-		Suit tempSuit;
+    private Suit extractSuit(String card) {
+        char ch = card.charAt(1);
+        Suit tempSuit;
 
-		switch (ch) {
-		case 'C':
-			tempSuit = Suit.CLUBS;
-			break;
-		case 'D':
-			tempSuit = Suit.DIAMONDS;
-			break;
-		case 'H':
-			tempSuit = Suit.HEARTS;
-			break;
-		case 'S':
-			tempSuit = Suit.SPADES;
-			break;
-		default:
-			tempSuit = null;
-			break;
-		}
+        switch (ch) {
+        case 'C':
+            tempSuit = Suit.CLUBS;
+            break;
+        case 'D':
+            tempSuit = Suit.DIAMONDS;
+            break;
+        case 'H':
+            tempSuit = Suit.HEARTS;
+            break;
+        case 'S':
+            tempSuit = Suit.SPADES;
+            break;
+        default:
+            tempSuit = null;
+            break;
+        }
 
-		return tempSuit;
-	}
+        return tempSuit;
+    }
 
-	public Value getValue() {
-		return value;
-	}
+    public Value getValue() {
+        return value;
+    }
 
-	public Suit getSuit() {
-		return suit;
-	}
+    public Suit getSuit() {
+        return suit;
+    }
 
 }
--- src/test/java/poker/CardTest.java
+++ src/test/java/poker/CardTest.java
@@ -5,61 +5,61 @@ import static org.junit.Assert.*;
 
 public class CardTest {
 
-	@Test
-	public void testCardGetValue() {
-		Card cardUnderTest = new Card("2C");
-		assertEquals(Card.Value.TWO, cardUnderTest.getValue());
+    @Test
+    public void testCardGetValue() {
+        Card cardUnderTest = new Card("2C");
+        assertEquals(Card.Value.TWO, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("3C");
-		assertEquals(Card.Value.THREE, cardUnderTest.getValue());
+        cardUnderTest = new Card("3C");
+        assertEquals(Card.Value.THREE, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("4C");
-		assertEquals(Card.Value.FOUR, cardUnderTest.getValue());
+        cardUnderTest = new Card("4C");
+        assertEquals(Card.Value.FOUR, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("5C");
-		assertEquals(Card.Value.FIVE, cardUnderTest.getValue());
+        cardUnderTest = new Card("5C");
+        assertEquals(Card.Value.FIVE, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("6C");
-		assertEquals(Card.Value.SIX, cardUnderTest.getValue());
+        cardUnderTest = new Card("6C");
+        assertEquals(Card.Value.SIX, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("7C");
-		assertEquals(Card.Value.SEVEN, cardUnderTest.getValue());
+        cardUnderTest = new Card("7C");
+        assertEquals(Card.Value.SEVEN, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("8C");
-		assertEquals(Card.Value.EIGHT, cardUnderTest.getValue());
+        cardUnderTest = new Card("8C");
+        assertEquals(Card.Value.EIGHT, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("9C");
-		assertEquals(Card.Value.NINE, cardUnderTest.getValue());
+        cardUnderTest = new Card("9C");
+        assertEquals(Card.Value.NINE, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("TC");
-		assertEquals(Card.Value.TEN, cardUnderTest.getValue());
+        cardUnderTest = new Card("TC");
+        assertEquals(Card.Value.TEN, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("JC");
-		assertEquals(Card.Value.JACK, cardUnderTest.getValue());
+        cardUnderTest = new Card("JC");
+        assertEquals(Card.Value.JACK, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("QC");
-		assertEquals(Card.Value.QUEEN, cardUnderTest.getValue());
+        cardUnderTest = new Card("QC");
+        assertEquals(Card.Value.QUEEN, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("KC");
-		assertEquals(Card.Value.KING, cardUnderTest.getValue());
+        cardUnderTest = new Card("KC");
+        assertEquals(Card.Value.KING, cardUnderTest.getValue());
 
-		cardUnderTest = new Card("AC");
-		assertEquals(Card.Value.ACE, cardUnderTest.getValue());
-	}
+        cardUnderTest = new Card("AC");
+        assertEquals(Card.Value.ACE, cardUnderTest.getValue());
+    }
 
-	@Test
-	public void testCardGetSuit() {
-		Card cardUnderTest = new Card("2C");
-		assertEquals(Card.Suit.CLUBS, cardUnderTest.getSuit());
+    @Test
+    public void testCardGetSuit() {
+        Card cardUnderTest = new Card("2C");
+        assertEquals(Card.Suit.CLUBS, cardUnderTest.getSuit());
 
-		cardUnderTest = new Card("2D");
-		assertEquals(Card.Suit.DIAMONDS, cardUnderTest.getSuit());
+        cardUnderTest = new Card("2D");
+        assertEquals(Card.Suit.DIAMONDS, cardUnderTest.getSuit());
 
-		cardUnderTest = new Card("2H");
-		assertEquals(Card.Suit.HEARTS, cardUnderTest.getSuit());
+        cardUnderTest = new Card("2H");
+        assertEquals(Card.Suit.HEARTS, cardUnderTest.getSuit());
 
-		cardUnderTest = new Card("2S");
-		assertEquals(Card.Suit.SPADES, cardUnderTest.getSuit());
-	}
+        cardUnderTest = new Card("2S");
+        assertEquals(Card.Suit.SPADES, cardUnderTest.getSuit());
+    }
 
 }
