8b4aac2492567cc8242a183fe9177a90f6d5f812
victorgse <victorgse@gmail.com>
2015-09-21 23:14:02 +0100

    Remove empty methods to satisfy SonarQube

--- src/test/java/poker/HandTest.java
+++ src/test/java/poker/HandTest.java
@@ -2,19 +2,9 @@ package poker;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class HandTest {
-
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
 
     @Test
     public void testHandContains() {
